5039320ec00b8c04981de5b13b86c857a594f2c7
chuckha <chuckha@hey.com>
2024-03-02 14:06:12 -0800

    feat: there is no verison information available

--- Casks/h/hey-email.rb
+++ Casks/h/hey-email.rb
@@ -1,9 +1,8 @@
 cask "hey-email" do
     arch arm: "aarch64", intel: "x86_64"
 
-    version "1.2.14"
-    sha256 arm:   "c9e795ef646164e11d065d9829fff9cfab768f8848ce737096bae9a31b3e56b8",
-           intel: "790aab1e7ff2466cecf8241922946ef0233c3189e531b600c21cc33b39a33594"
+    version :latest
+    sha256 :no_check
 
     url "https://hey-desktop.s3.amazonaws.com/HEY#{arch == "aarch64" ? "-arm64" : ""}.dmg",
         verified: "hey-desktop.s3.amazonaws.com/"
