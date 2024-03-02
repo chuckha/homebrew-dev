095dd1a46cbef62edead2d6827bed051d3ac22ec
chuckha <chuckha@hey.com>
2024-03-02 10:49:08 -0800

    feat: add HEY email cask

---
+++ Casks/h/hey-email.rb
@@ -0,0 +1,17 @@
+cask "hey-email" do
+    arch arm: "aarch64", intel: "x86_64"
+
+    version "1.2.14"
+    sha256 arm:   "c9e795ef646164e11d065d9829fff9cfab768f8848ce737096bae9a31b3e56b8",
+           intel: "790aab1e7ff2466cecf8241922946ef0233c3189e531b600c21cc33b39a33594"
+
+    url "https://hey-desktop.s3.amazonaws.com/HEY#{arch == "arm" ? "-arm64" : ""}.dmg",
+        verified: "hey-desktop.s3.amazonaws.com/"
+    name "HEY Email"
+    desc "HEY email client"
+    homepage "https://www.hey.com"
+
+    depends_on macos: ">= :catalina"
+
+    app "HEY.app"
+  end
