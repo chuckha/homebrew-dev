047ae3a0cdcb6bcbc783defdb02d685cf27abac8
chuckha <chuckha@hey.com>
2024-03-02 13:59:47 -0800

    fix: improve arch

--- Casks/h/hey-email.rb
+++ Casks/h/hey-email.rb
@@ -5,7 +5,7 @@ cask "hey-email" do
     sha256 arm:   "c9e795ef646164e11d065d9829fff9cfab768f8848ce737096bae9a31b3e56b8",
            intel: "790aab1e7ff2466cecf8241922946ef0233c3189e531b600c21cc33b39a33594"
 
-    url "https://hey-desktop.s3.amazonaws.com/HEY#{arch == "aarch" ? "-arm64" : ""}.dmg",
+    url "https://hey-desktop.s3.amazonaws.com/HEY#{arch == "aarch64" ? "-arm64" : ""}.dmg",
         verified: "hey-desktop.s3.amazonaws.com/"
     name "HEY Email"
     desc "HEY email client"
